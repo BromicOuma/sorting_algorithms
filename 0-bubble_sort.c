4e57059a3e53c3539c1e8527d72361e9d436d5dd
Bromic Ouma <oumabromic7@gmail.com>
2023-04-21 05:46:50 -0700

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort arrays of integer in ascending order
+ *
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return - nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+			}
+		}
+		print_array(array, size);
+
+	}
+
+
+
+
+}
+
